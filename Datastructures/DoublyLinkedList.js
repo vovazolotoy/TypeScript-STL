2feaf6eed7f49fb1a2a2a2a3cf01c6729b8ddd69
Vova Zolotoy <vovazolotoy@gmail.com>
2014-09-28 17:53:18 -0700

    DLL core methods

--- Datastructures/DoublyLinkedList.js
+++ Datastructures/DoublyLinkedList.js
@@ -4,6 +4,141 @@
 var DoublyLinkedList = (function () {
     function DoublyLinkedList() {
     }
+    /**
+    * Insert a new value at the specified index
+    *
+    * @param index The index where the new value is to be inserted.
+    * @param value The new value for the index.
+    * @return void
+    */
+    DoublyLinkedList.prototype.add = function (index, value) {
+    };
+
+    /**
+    * Pops a node from the end of the doubly linked list
+    *
+    * @return any  The value of the popped node.
+    */
+    DoublyLinkedList.prototype.pop = function () {
+    };
+
+    /**
+    * Shifts a node from the beginning of the doubly linked list
+    *
+    * @return any  The value of the shifted node.
+    */
+    DoublyLinkedList.prototype.shift = function () {
+    };
+
+    /**
+    * Pushes an element at the end of the doubly linked list
+    *
+    * @param value The value to push.
+    * @return void
+    */
+    DoublyLinkedList.prototype.push = function (value) {
+    };
+
+    /**
+    * Prepends the doubly linked list with an element
+    *
+    * @param value The value to unshift.
+    * @return void
+    */
+    DoublyLinkedList.prototype.unshift = function (value) {
+    };
+
+    /**
+    * Peeks at the node from the end of the doubly linked list
+    *
+    * @return any  The value of the last node.
+    */
+    DoublyLinkedList.prototype.top = function () {
+    };
+
+    /**
+    * Peeks at the node from the beginning of the doubly linked list
+    *
+    * @return any  The value of the first node.
+    */
+    DoublyLinkedList.prototype.bottom = function () {
+    };
+
+    /**
+    * Counts the number of elements in the doubly linked list
+    *
+    * @return number the number of elements in the doubly linked list.
+    */
+    DoublyLinkedList.prototype.count = function () {
+        return 0;
+    };
+
+    /**
+    * Checks whether the doubly linked list is empty
+    *
+    * @return boolean whether the doubly linked list is empty.
+    */
+    DoublyLinkedList.prototype.isEmpty = function () {
+        return true;
+    };
+
+    /**
+    * Rewind iterator back to the start
+    *
+    * @return void
+    */
+    DoublyLinkedList.prototype.rewind = function () {
+    };
+
+    /**
+    * Return current array entry
+    *
+    * @return any  The current node value.
+    */
+    DoublyLinkedList.prototype.current = function () {
+    };
+
+    /**
+    * Return current node index
+    *
+    * @return any  The current node index.
+    */
+    DoublyLinkedList.prototype.key = function () {
+    };
+
+    /**
+    * Move to next entry
+    *
+    * @return void
+    */
+    DoublyLinkedList.prototype.next = function () {
+    };
+
+    /**
+    * Move to previous entry
+    *
+    * @return void
+    */
+    DoublyLinkedList.prototype.prev = function () {
+    };
+
+    /**
+    * Check whether the doubly linked list contains more nodes
+    *
+    * @return boolean true if the doubly linked list contains any more nodes, false otherwise.
+    */
+    DoublyLinkedList.prototype.valid = function () {
+        return false;
+    };
+
+    /**
+    * Serializes the list
+    *
+    * @return string The serialized string.
+    */
+    DoublyLinkedList.prototype.toString = function () {
+        return "";
+    };
     return DoublyLinkedList;
 })();
 //# sourceMappingURL=DoublyLinkedList.js.map
--- Datastructures/DoublyLinkedList.ts
+++ Datastructures/DoublyLinkedList.ts
@@ -3,4 +3,151 @@
  */
 class DoublyLinkedList {
 
+    /**
+     * Insert a new value at the specified index
+     *
+     * @param index The index where the new value is to be inserted.
+     * @param value The new value for the index.
+     * @return void
+     */
+    public add(index:any, value:any):void {
+
+    }
+
+    /**
+     * Pops a node from the end of the doubly linked list
+     *
+     * @return any  The value of the popped node.
+     */
+    public pop():any {
+
+    }
+
+    /**
+     * Shifts a node from the beginning of the doubly linked list
+     *
+     * @return any  The value of the shifted node.
+     */
+    public shift():any {
+
+    }
+
+    /**
+     * Pushes an element at the end of the doubly linked list
+     *
+     * @param value The value to push.
+     * @return void
+     */
+    public push (value:any):void {
+
+    }
+
+    /**
+     * Prepends the doubly linked list with an element
+     *
+     * @param value The value to unshift.
+     * @return void
+     */
+    public unshift(value:any):void {
+
+    }
+
+    /**
+     * Peeks at the node from the end of the doubly linked list
+     *
+     * @return any  The value of the last node.
+     */
+    public top():any {
+
+    }
+
+    /**
+     * Peeks at the node from the beginning of the doubly linked list
+     *
+     * @return any  The value of the first node.
+     */
+    public bottom():any {
+
+    }
+
+    /**
+     * Counts the number of elements in the doubly linked list
+     *
+     * @return number the number of elements in the doubly linked list.
+     */
+    public count():number {
+        return 0;
+    }
+
+    /**
+     * Checks whether the doubly linked list is empty
+     *
+     * @return boolean whether the doubly linked list is empty.
+     */
+    public isEmpty():boolean {
+        return true;
+    }
+
+    /**
+     * Rewind iterator back to the start
+     *
+     * @return void
+     */
+    public rewind():void {
+
+    }
+
+    /**
+     * Return current array entry
+     *
+     * @return any  The current node value.
+     */
+    public current():any {
+
+    }
+
+    /**
+     * Return current node index
+     *
+     * @return any  The current node index.
+     */
+    public key():any {
+
+    }
+
+    /**
+     * Move to next entry
+     *
+     * @return void
+     */
+    public next():void {
+
+    }
+
+    /**
+     * Move to previous entry
+     *
+     * @return void
+     */
+    public prev():void {
+
+    }
+
+    /**
+     * Check whether the doubly linked list contains more nodes
+     *
+     * @return boolean true if the doubly linked list contains any more nodes, false otherwise.
+     */
+    public valid():boolean {
+        return false;
+    }
+
+    /**
+     * Serializes the list
+     *
+     * @return string The serialized string.
+     */
+    public  toString():string {
+        return "";
+    }
 }
