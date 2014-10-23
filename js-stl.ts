a4ecef68e3152b1c7f510b380af45fb10530cd80
Vova Zolotoy <vovazolotoy@gmail.com>
2014-10-23 00:06:59 -0700

    module reorganizations

--- js-stl.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="Datastructures/DoublyLinkedList.ts"/>
-import DoublyLinkedList = require("./Datastructures/DoublyLinkedList");
-// vova zoloyo
-export var dll = DoublyLinkedList;
