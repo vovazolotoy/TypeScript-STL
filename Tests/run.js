77e03ddf0f99452db10ecd5671c2af7aac81faab
Vova Zolotoy <vovazolotoy@gmail.com>
2014-11-07 23:50:50 -0800

    tests

--- Tests/run.js
+++ Tests/run.js
@@ -1,4 +1,26 @@
 
-console.log('Test running');
+console.log('Testing...');
 
-// throw new Error('Just standard Exception');
+/**
+ * Base asserting
+ *
+ * @param condition
+ * @param message
+ */
+function assert(condition, message) {
+	if (!condition) {
+		throw message || "Assertion failed";
+	}
+}
+
+DoublyLinkedList = require('./../Datastructures/DoublyLinkedList.js');
+Stack = require('./../Datastructures/Stack.js');
+Queue = require('./../Datastructures/Queue.js');
+
+var stack = new Stack();
+var stack2 = new Stack();
+
+assert(JSON.stringify(stack.toArray()) === JSON.stringify([]));
+assert(JSON.stringify(stack.toArray()) === JSON.stringify(stack2.toArray()));
+assert(JSON.stringify(stack) === JSON.stringify(stack2));
+assert(JSON.stringify(stack.toArray()) !== JSON.stringify([1]));
