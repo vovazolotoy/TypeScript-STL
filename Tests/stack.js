b76942fc9080e5d22bc0507647f26f1ed0699442
Vova Zolotoy <vovazolotoy@gmail.com>
2014-11-22 23:10:36 -0800

    Test Stack::push

--- Tests/stack.js
+++ Tests/stack.js
@@ -10,5 +10,20 @@ var Stack = require('./../Datastructures/Stack.js');
 // testing stacks
 var stack = new Stack();
 var stack2 = new Stack();
-var stack3 = new Stack();
-var stack4 = new Stack();
+
+// Stack::push
+assert(stack.push(1) === undefined);
+assert(JSON.stringify(stack.toArray()) === JSON.stringify([1]));
+assert(stack.push(2) === undefined);
+assert(JSON.stringify(stack.toArray()) === JSON.stringify([1, 2]));
+assert(stack.push(3) === undefined);
+assert(JSON.stringify(stack.toArray()) === JSON.stringify([1, 2, 3]));
+assert(stack.push(4) === undefined);
+assert(JSON.stringify(stack.toArray()) === JSON.stringify([1, 2, 3, 4]));
+assert(stack2.push() === undefined);
+assert(JSON.stringify(stack2.toArray()) === JSON.stringify([undefined]));
+assert(stack2.push(null) === undefined);
+assert(JSON.stringify(stack2.toArray()) === JSON.stringify([undefined, null]));
+assert(stack2.push(false) === undefined);
+assert(JSON.stringify(stack2.toArray()) === JSON.stringify([undefined, null, false]));
+
