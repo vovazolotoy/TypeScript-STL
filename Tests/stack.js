904eb1342c3f72a3f79269e6b4d3bec2e7ff4f13
Vova Zolotoy <vovazolotoy@gmail.com>
2014-11-22 23:22:21 -0800

    Test Stack::pop

--- Tests/stack.js
+++ Tests/stack.js
@@ -27,3 +27,18 @@ assert(JSON.stringify(stack2.toArray()) === JSON.stringify([undefined, null]));
 assert(stack2.push(false) === undefined);
 assert(JSON.stringify(stack2.toArray()) === JSON.stringify([undefined, null, false]));
 
+// Stack::pop
+assert(stack.pop() === 4);
+assert(JSON.stringify(stack.toArray()) === JSON.stringify([1, 2, 3]));
+assert(stack.pop() === 3);
+assert(JSON.stringify(stack.toArray()) === JSON.stringify([1, 2]));
+assert(stack.pop() === 2);
+assert(JSON.stringify(stack.toArray()) === JSON.stringify([1]));
+assert(stack.pop() === 1);
+assert(JSON.stringify(stack.toArray()) === JSON.stringify([]));
+assert(stack2.pop() === false);
+assert(JSON.stringify(stack2.toArray()) === JSON.stringify([undefined, null]));
+assert(stack2.pop() === null);
+assert(JSON.stringify(stack2.toArray()) === JSON.stringify([undefined]));
+assert(stack2.pop() === undefined);
+assert(JSON.stringify(stack2.toArray()) === JSON.stringify([]));
