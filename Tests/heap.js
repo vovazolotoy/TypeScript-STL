fd7bfcffca0bcd194deeca250b8cc82687355d00
Vova Zolotoy <vovazolotoy@gmail.com>
2014-12-01 17:05:35 -0800

    Heap Test

--- Tests/heap.js
+++ Tests/heap.js
@@ -10,3 +10,26 @@ var Heap = require('./../Datastructures/Heap.js');
 // testing Heap
 var heap = new Heap();
 var heap2 = new Heap();
+
+// Heap::insert
+assert(heap.insert(1) === undefined);
+assert(heap.insert(2) === undefined);
+assert(heap.insert(3) === undefined);
+assert(heap.insert(4) === undefined);
+assert(heap.insert(5) === undefined);
+assert(heap.insert(10) === undefined);
+
+/*
+ compare
+ count
+ current
+ extract
+ isEmpty
+ key
+ next
+ recoverFromCorruption
+ rewind
+ top
+ toString
+ valid
+ */
