5238d06aca39c8eb3430e5d88076a38d071dee15
Vova Zolotoy <vovazolotoy@gmail.com>
2014-12-01 17:11:56 -0800

    Heap::toArray tests

--- Tests/heap.js
+++ Tests/heap.js
@@ -12,12 +12,19 @@ var heap = new Heap();
 var heap2 = new Heap();
 
 // Heap::insert
+// Heap::toArray
 assert(heap.insert(1) === undefined);
+assert(JSON.stringify(heap.toArray()) === JSON.stringify([1]));
 assert(heap.insert(2) === undefined);
+assert(JSON.stringify(heap.toArray()) === JSON.stringify([1,2]));
 assert(heap.insert(3) === undefined);
+assert(JSON.stringify(heap.toArray()) === JSON.stringify([1,2,3]));
 assert(heap.insert(4) === undefined);
+assert(JSON.stringify(heap.toArray()) === JSON.stringify([1,2,3,4]));
 assert(heap.insert(5) === undefined);
+assert(JSON.stringify(heap.toArray()) === JSON.stringify([1,2,3,4,5]));
 assert(heap.insert(10) === undefined);
+assert(JSON.stringify(heap.toArray()) === JSON.stringify([1,2,3,4,5,10]));
 
 /*
  compare
