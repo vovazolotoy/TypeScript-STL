a196e2d0b04590c1b46981c773145c7469f46625
Vova Zolotoy <vovazolotoy@gmail.com>
2015-02-03 00:36:53 -0800

    // PriorityQueue.top Tests

--- Tests/priorityqueue.js
+++ Tests/priorityqueue.js
@@ -10,13 +10,21 @@ var PriorityQueue = require('./../Datastructures/PriorityQueue.js');
 // testing PriorityQueue
 var queue = new PriorityQueue();
 
+// PriorityQueue.enqueue
+// PriorityQueue.dequeue
+// PriorityQueue.top
 queue.enqueue('C', 0.000001);
+assert(queue.top() !== 'C2');
+assert(queue.top() === 'C');
 queue.enqueue('C2', 2);
+assert(queue.top() === 'C2');
 queue.enqueue('C3', 3);
 queue.enqueue('C1', 1);
 queue.enqueue('Cmin', 0.01);
 queue.enqueue('C4', 0.1);
+assert(queue.top() === 'C3');
 assert(queue.dequeue() === 'C3');
+assert(queue.top() === 'C2');
 assert(queue.dequeue() === 'C2');
 assert(queue.dequeue() === 'C1');
 assert(queue.dequeue() === 'C4');
